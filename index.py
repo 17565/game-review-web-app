b7687bb8f7ddfe1ca51fec261f77903f430a7097
harrison bray-lim <17565@burnside.school.nz>
2021-04-08 11:51:29 +1200

    fixed datacbase

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 """ this is  the main set of code form my web app"""
+from datetime import datetime
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -10,8 +11,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.games'
 db = SQLAlchemy(app)
 
 class User(db.Model):
-    id = db.column(db.integer, primary_key=True)
-    game = db.column(db.string(50))
+    id = db.Column(db.Integer, primary_key=True)
+    game = db.Column(db.String(50))
     image = db.Column(db.String(50))
     gener_id = db.Column(db.String(50))
     release_date = db.Column(db.String(50))
@@ -27,3 +28,19 @@ def Index():
 #runs app
 if __name__ == "__main__":
     app.run(debug=True)
+
+#>>> from test_code import db
+#>>> db.create_all()
+
+#>>> from test_code import User
+#>>> bob = User(name = "starcraft")
+#>>> db.session.add(bob)
+#>>> db.session.commit()
+#>>> from test_code import db,User
+#>>> sc = User.query.filter_by(id = 1).first()
+#>>> sc
+#<User 1>
+
+#>>> sc = User.query.filter_by(id = 2).first()
+#>>> db.session.delete(sc)     
+#>>> db.session.commit()
