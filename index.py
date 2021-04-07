8cd64dab18389e519c2e4f3132296a172beae6cf
harrison bray-lim <17565@burnside.school.nz>
2021-04-08 11:03:18 +1200

    first commit

--- index.py
+++ index.py
@@ -1,1 +1,23 @@
-print("hello world")
+""" this is  the main set of code form my web app"""
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.column(db.integer, primary_key=True)
+    game_name = db.column(db.string(80))
+    image_name = db.Column(db.String(50))
+    
+@app.route("/")
+def Index():
+    return "<h1>hello world<?h1>"
+
+#runs app
+if __name__ == "__main__":
+    app.run(debug=True)
