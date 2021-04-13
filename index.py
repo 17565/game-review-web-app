e070101dbf2eaf9f7abf5a81edf68e1cb5b04ead
harrison bray-lim <17565@burnside.school.nz>
2021-04-13 12:41:59 +1200

    fixed date base

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 """ this is  the main set of code form my web app"""
+
 from datetime import datetime
 
 from flask import Flask
@@ -6,19 +7,29 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.games'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///games.db'
 
 db = SQLAlchemy(app)
-
-class User(db.Model):
+#game table
+class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     game = db.Column(db.String(50))
-    image = db.Column(db.String(50))
-    gener_id = db.Column(db.String(50))
-    release_date = db.Column(db.String(50))
-    publisher = db.Column(db.String(50))
-    developer = db.Column(db.String(50))
-
+    reviews = db.relationship('Review', backref='game', lazy=True)
+#    image = db.Column(db.String(50))
+#    release_date = db.Column(db.String(50))
+#    gener_id = db.Column(db.Integer, db.ForeignKey(''),
+#        nullable=False)
+#    publisher_id = db.Column(db.Integer, db.ForeignKey(''),
+#        nullable=False)
+#    developer_id = db.Column(db.Integer, db.ForeignKey(''),
+#        nullable=False)
+#review table
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    review = db.Column(db.String(50))
+#    release_date = db.Column(db.DateTime, default=datetime.now)
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'),
+        nullable=False)
 
     
 @app.route("/")
@@ -37,10 +48,28 @@ if __name__ == "__main__":
 #>>> db.session.add(bob)
 #>>> db.session.commit()
 #>>> from test_code import db,User
-#>>> sc = User.query.filter_by(id = 1).first()
+#>>> sc = User.query.filter_by(id = 1).first() 
 #>>> sc
 #<User 1>
 
 #>>> sc = User.query.filter_by(id = 2).first()
 #>>> db.session.delete(sc)     
 #>>> db.session.commit()
+
+# = db.Column(db.Integer, db.ForeignKey(''),
+#        nullable=False)
+
+# = db.relationship('', backref='', lazy=True)
+
+"""gener table
+class Gener(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    gener = db.Column(db.string(50))
+#publisher table
+class Publisher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    publisher = db.Column(db.string(50))
+#developer table
+class Developer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    developer = db.Column(db.string(50))"""
